381af570ece27674935a7691961b534e4b61c405
zalerr <94384337+zalerr@users.noreply.github.com>
2023-12-06 13:36:49 +0100

    Main update v2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,14 @@ public class Main {
     public static void add(int a, int b) {
         System.out.println(a + b);
     }
+    public static void substract(int a, int b) {
+        System.out.println(a - b);
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int a = 5;
         int b = 10;
         add(a, b);
+        substract(a, b);
     }
 }
