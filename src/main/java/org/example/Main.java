3648c7ba97a5630d95f0e3058523f5db2108659d
zalerr <94384337+zalerr@users.noreply.github.com>
2023-12-06 13:52:49 +0100

    Main multiply method and Test2 class added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,9 @@ public class Main {
     public static void substract(int a, int b) {
         System.out.println(a - b);
     }
+    public static void multiply(int a, int b) {
+        System.out.println(a*b);
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int a = 5;
