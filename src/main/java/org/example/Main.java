99d2da650130618846f9ad7e0248edd920bb1200
zalerr <94384337+zalerr@users.noreply.github.com>
2023-12-06 13:34:29 +0100

    Main update and Test class added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
 public class Main {
+    public static void add(int a, int b) {
+        System.out.println(a + b);
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a = 5;
+        int b = 10;
+        add(a, b);
     }
 }
